856d69b9f64ccb897ae94a5d779fd8ca1a5abff7
Ponacko <ponacko@gmail.com>
2024-09-18 15:05:43 +0200

    add constants for Activation

--- app/src/main/java/com/example/scratchcard/Constants.kt
+++ app/src/main/java/com/example/scratchcard/Constants.kt
@@ -1,3 +1,5 @@
 package com.example.scratchcard
 
 const val SCRATCH_DELAY_MS: Long = 2000
+const val MIN_VALUE_FOR_ACTIVATION: Long = 277028
+const val PLATFORM_ACTIVATION_FIELD: String = "android"
